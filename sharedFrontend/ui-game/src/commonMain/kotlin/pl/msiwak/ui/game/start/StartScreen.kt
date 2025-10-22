a4e9905325aa024929b013d59ea959ad8cb975dc
msiwak <marcinsiwak15@gmail.com>
2025-10-22 14:08:04 +0200

    changed start screen theme

--- sharedFrontend/ui-game/src/commonMain/kotlin/pl/msiwak/ui/game/start/StartScreen.kt
+++ sharedFrontend/ui-game/src/commonMain/kotlin/pl/msiwak/ui/game/start/StartScreen.kt
@@ -1,17 +1,15 @@
 package pl.msiwak.ui.game.start
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -19,10 +17,15 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import cardsthegame.sharedfrontend.common_resources.generated.resources.Res
+import cardsthegame.sharedfrontend.common_resources.generated.resources.img_backround
+import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.koinInject
+import pl.msiwak.ui.game.component.InputField
 
 @Composable
 fun StartScreen(
@@ -35,11 +38,15 @@ fun StartScreen(
     Scaffold { paddingValues ->
         Box(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-                .padding(16.dp),
+                .fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(Res.drawable.img_backround),
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+            )
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.spacedBy(16.dp)
@@ -50,20 +57,13 @@ fun StartScreen(
                     fontWeight = FontWeight.Bold,
                     textAlign = TextAlign.Center
                 )
-                Text(
-                    text = state.value.gameIpAddress?.let { "Game IP: $it" } ?: "No game available",
-                    style = MaterialTheme.typography.h4,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center
-                )
 
                 Spacer(modifier = Modifier.height(32.dp))
 
-                OutlinedTextField(
+                InputField(
                     value = state.value.playerName,
                     onValueChange = { viewModel.onUiAction(StartUiAction.OnPlayerNameChanged(it)) },
-                    label = { Text("Player Name") },
-                    modifier = Modifier.fillMaxWidth(),
+                    placeholder = "Player Name",
                     enabled = !state.value.isLoading
                 )
 
